908f5e613b50f5782aa41593ec3bba9997f91f7c
Abdi Kumela <abdikumela036@gmail.com>
2024-07-20 13:59:55 +0300

    Added Error handling

--- Client/src/pages/Laptops.jsx
+++ Client/src/pages/Laptops.jsx
@@ -1,7 +1,41 @@
-import products from "../../public/products.json";
+import { useEffect, useState } from "react";
 import Item from "../components/Items";
 
 export default function Laptops() {
+  const [products, setProducts] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const res = await fetch("http://localhost:8000/products");
+        if (!res.ok) {
+          throw new Error("Somethimg went Wrong while fetching data.");
+        }
+        const data = await res.json();
+        setProducts(data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    }
+    fetchData();
+  }, []);
+
+  if (loading) {
+    return (
+      <div className="flex justify-center items-center min-h-screen">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
     <div className="container mx-auto py-8">
       <header className="text-center mb-12">
